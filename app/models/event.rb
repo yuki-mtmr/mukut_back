c1fdb6970b1aeac32559ab369eec56f62171f572
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-02-24 18:52:53 +0900

    add event association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+  has_many :event_member
+  has_many :users, through: :event_member
+
+  validates :event_title, : true
+  validates :overview, : true
+  validates :start_datetime, : true
+  validates :end_datetime, : true
+  validates :place, : true
+  validates :max_member, : true
 end
