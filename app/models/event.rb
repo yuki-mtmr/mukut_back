f6dc75cc673da8dfa5da4e7e2d634bacc9f4822d
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-02-25 11:08:49 +0900

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,10 +2,4 @@ class Event < ApplicationRecord
   has_many :event_members
   has_many :users, through: :event_member
 
-  validates :event_title, presence: true
-  validates :overview, presence: true
-  validates :start_datetime, presence: true
-  validates :end_datetime, presence: true
-  validates :place, presence: true
-  validates :max_member, presence: true
 end
