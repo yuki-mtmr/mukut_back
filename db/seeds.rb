0b4082b0d12ba92323ebef9bd70d6736db02dba8
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-02-25 10:36:16 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Event.create!(
   owner_id: "5",
-  event_title: "kkk",
+  event_title: "サッカー",
   overview: "asdfghj",
   start_datetime:"2020-4-5 15:00",
   end_datetime:"2020-4-5 17:00",
@@ -8,3 +8,14 @@ Event.create!(
   fee:"3000",
   max_member:"20000"
   )
+
+  Event.create!(
+    owner_id: "3",
+    event_title: "プログラミング",
+    overview: "asdfghj",
+    start_datetime:"2020-4-6 15:00",
+    end_datetime:"2020-4-6 17:00",
+    place:"Osaka",
+    fee:"3000",
+    max_member:"10"
+    )
