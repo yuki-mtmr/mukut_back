8223e9c5fb2db050cd7662d0194661212d091705
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-03-04 12:28:01 +0900

    add_events_validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,9 @@ class Event < ApplicationRecord
   has_many :event_members
   has_many :users, through: :event_member
 
-  validates :event_title, presence: true
-  validates :place, presence: true
-  validates :max_member, presence: true
+  validates :event_title, presence: true, length: { maximum: 255 }
+  validates :place, presence: true, length: { maximum: 255 }
+  validates :max_member, presence: true, length: { maximum: 5 }
   validates :overview, presence: true
 
 end
